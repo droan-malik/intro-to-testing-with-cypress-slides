85029f2eab0fb56f4a647d0fc906c96233b27c3d
Droan Malik <droan.malik@nutanix.com>
2021-02-09 16:45:52 +0530

    🐣

---
+++ Link.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Link = (props) => {
+  return (
+    <a {...props} style={{color:'white'}} target="_blank">
+    </a>
+  )
+}
+
+export default Link;
---
+++ hr.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hr = () => {
+  return (
+    <hr style={{margin:0}}/>
+  )
+}
+
+export default Hr
